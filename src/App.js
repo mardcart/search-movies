84af7a76b2c8a933ed943714c62a6ea3db8027ab
marco cardenas <mardcart.9@gmail.com>
2022-10-05 01:08:50 -0300

    se anade una api de peliculas

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import './App.css';
 import 'bulma/css/bulma.css'
 import { Title } from './Componentes/Title';
 import { SearchForm } from './Componentes/SearchForm';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  state = {
+    results : []
+  }
+  _handleResults = (results) => {
+    this.setState({results})
+  }
+
+  _renderResults = () => {
+    const { results } =  this.state
+    return results.map(
+      movie => { return <p key={movie.imdbID}>{movie.Title}</p> }
+    )
+
+  }
+  render(){
   return (
     <div className="App">
       <Title>Search movies</Title>
       <div className='SearchForm-wrapper'>
-        <SearchForm/>
+        <SearchForm onResults = {this._handleResults} />
       </div>
+      {this.state.results.length === 0
+        ? <p>sin resultado</p>
+        //: <p>con resultado</p>
+        : this._renderResults()
+      }
     </div>
   );
+  }
 }
 
 export default App;
--- src/Componentes/SearchForm.jsx
+++ src/Componentes/SearchForm.jsx
@@ -1,14 +1,41 @@
 import { Component } from "react";
 
+const API_KEY = '3a124f3e'
+
 export class SearchForm extends Component{
+
+    state = {
+        inputMovie: ''
+    }
+
+    _handleChange=(e)=>{
+        this.setState({inputMovie:e.target.value})
+    }
+    _handleSubmit=(e)=>{
+        e.preventDefault()
+        //alert(this.state.inputMovie)
+        const { inputMovie } = this.state
+        
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+        .then(res=> res.json())
+        .then(results =>{
+            // console.log(results)
+            const {Search , totalResults} = results
+            console.log({Search,totalResults})
+            this.props.onResults(Search)
+        })
+    }
+
     render(){
         return(
+            <form onSubmit={this._handleSubmit}>
             <div className="field has-addons">
             <div className="control">
                 <input 
                     className="input" 
                     type="text" 
-                    placeholder="Find a repository"
+                    onChange={this._handleChange}
+                    placeholder="movie to search"
                 />
             </div>
             <div className="control">
@@ -17,6 +44,8 @@ export class SearchForm extends Component{
                 </button>
             </div>
             </div>
-        )
+
+            </form>
+            )
     }
 }
